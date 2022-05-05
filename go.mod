f4c4c2a12a75f4a3a928ba3a323ef235d8b0eba3
Alicia Thoney <athoney@uwyo.edu>
2022-05-05 08:08:45 -0600

    Init

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/athoney/finalGame
+
+go 1.17
+
+require (
+	github.com/bhendo/go-powershell v0.0.0-20190719160123-219e7fb4e41e // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20220320163800-277f93cfa958 // indirect
+	github.com/hajimehoshi/ebiten v1.12.12 // indirect
+	github.com/hajimehoshi/ebiten/v2 v2.2.7 // indirect
+	github.com/jezek/xgb v1.0.0 // indirect
+	github.com/juju/errors v0.0.0-20220331221717-b38fca44723b // indirect
+	github.com/martinlindhe/inputbox v0.0.0-20210326232244-b26136a79ad0 // indirect
+	github.com/ryosama/go-sprite v0.0.0-20210215131658-bad96d1c278a // indirect
+	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
+	golang.org/x/exp/shiny v0.0.0-20220428152302-39d4317da171 // indirect
+	golang.org/x/image v0.0.0-20220413100746-70e8d0d3baa9 // indirect
+	golang.org/x/mobile v0.0.0-20220414153400-ce6a79cf6a13 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220502124256-b6088ccd6cba // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ hscan/hscan.go
@@ -0,0 +1,108 @@
+package hscan
+
+import (
+	"bufio"
+	"crypto/md5"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+)
+
+//==========================================================================\\
+
+var shalookup map[string]string
+var md5lookup map[string]string
+
+func GuessSingle(sourceHash string, filename string) string {
+
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+	returnVal := "Not Found"
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		password := scanner.Text()
+
+		length := len(sourceHash)
+
+		if length == 32 {
+			hash := fmt.Sprintf("%x", md5.Sum([]byte(password)))
+			if hash == sourceHash {
+				//fmt.Printf("[+] Password found (MD5): %s\n", password)
+				returnVal = password
+			}
+		} else if length == 64 {
+			hash := fmt.Sprintf("%x", sha256.Sum256([]byte(password)))
+			if hash == sourceHash {
+				//fmt.Printf("[+] Password found (SHA-256): %s\n", password)
+				returnVal = password
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err)
+	}
+	return returnVal
+}
+
+func GenHashMaps(filename string) (int, int) {
+
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		password := scanner.Text()
+
+		go GetSHA(password)
+
+		go GetMD5(password)
+	}
+
+	return len(md5lookup), len(shalookup)
+	//TODO
+	//itterate through a file (look in the guessSingle function above)
+	//rather than check for equality add each hash:passwd entry to a map SHA and MD5 where the key = hash and the value = password
+	//TODO at the very least use go subroutines to generate the sha and md5 hashes at the same time
+	//OPTIONAL -- Can you use workers to make this even faster
+
+	//TODO create a test in hscan_test.go so that you can time the performance of your implementation
+	//Test and record the time it takes to scan to generate these Maps
+	// 1. With and without using go subroutines
+	// 2. Compute the time per password (hint the number of passwords for each file is listed on the site...)
+}
+
+func GetSHA(hash string) (string, error) {
+	password, ok := shalookup[hash]
+	if ok {
+		return password, nil
+
+	} else {
+
+		return "", errors.New("password does not exist")
+
+	}
+}
+
+func GetMD5(hash string) (string, error) {
+	password, ok := md5lookup[hash]
+	if ok {
+		return password, nil
+
+	} else {
+
+		return "", errors.New("password does not exist")
+
+	}
+}
